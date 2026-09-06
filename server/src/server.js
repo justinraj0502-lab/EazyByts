74e6565519271adf06767ca539bfd91b57e51418
justinraj0502-lab <justinraj0502@gmail.com>
2026-09-06 11:44:52 +0530

    Allow Netlify frontend in CORS

--- server/src/server.js
+++ server/src/server.js
@@ -19,16 +19,19 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "http://localhost:5175",
+  "http://localhost:5176",
+  "http://localhost:5177",
+  "http://localhost:5178",
+  "https://subtle-puffpuff-2466af.netlify.app",
+];
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "http://localhost:5175",
-      "http://localhost:5176",
-      "http://localhost:5177",
-      "http://localhost:5178",
-    ],
+    origin: allowedOrigins,
   })
 );
 
